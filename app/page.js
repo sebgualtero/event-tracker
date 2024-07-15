31019364e3974336b08baa812e2011e1163b9ada
Seb Gualtero <juansebastian.gualterovillamil@edu.sait.ca>
2024-07-15 13:32:16 -0600

    Added city state
    This state should be passed down to the event list and called with the search button

--- app/page.js
+++ app/page.js
@@ -7,15 +7,24 @@ import { useState } from "react";
 export default function Home() {
   const [city, setCity] = useState("");
 
-  function manageCityChange() {
-    setCity(document.getElementById("search").value);
-  }
+  const manageCityChange = (event) => {
+    setCity(event.target.value);
+  };
 
   console.log(city);
 
   return (
     <main>
       <nav></nav>
+      <form>
+        <label>Enter your city: </label>
+        <input
+          type="text"
+          className="color-black"
+          onChange={manageCityChange}
+        />
+        <button>Search!</button>
+      </form>
       <div>
         <EventList />
       </div>
