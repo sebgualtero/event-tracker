29f28b58d2d2a5f4d50489b26d2a7ed6ef015588
RikudoKen <141680797+RikudoKen@users.noreply.github.com>
2024-07-20 15:50:22 -0600

    Styling for search bar
    Search button styling prototype, but functionality is not done yet.

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import EventList from "./Components/event-list";
+import SearchBar from "./Components/search-bar";
 import { useState } from "react";
 
 export default function Home() {
@@ -13,25 +14,18 @@ export default function Home() {
 
   console.log(city);
 
+
   return (
     <main className="bg-slate-200 rounded-md">
       <nav></nav>
-      <div class="row">
-        <div class="col s12">
-          <div class="row">
-            <div class="input-field col s12">
-              <label for="autocomplete-input" className="text-gray-900">
-                Enter your city
-              </label>
-              <br></br>
-              <input type="text" id="autocomplete-input" class="autocomplete" />
-            </div>
-          </div>
-        </div>
+
+      <div>
+        <SearchBar />
       </div>
+
       <div className="bg-slate-400 border-4 m-3 rounded-md"></div>
 
-      <div className="row">
+      <div className="row ml-3">
         <EventList />
       </div>
     </main>
