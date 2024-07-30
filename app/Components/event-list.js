10a6fae2a5c7278633b009ac286ed5009e4a2ae8
Seb Gualtero <juansebastian.gualterovillamil@edu.sait.ca>
2024-07-30 17:48:57 -0600

    Added sorting to events
    Fixed time format, added buttons to filter by type of event, these are found in the API response under classifications

--- app/Components/event-list.js
+++ app/Components/event-list.js
@@ -18,6 +18,12 @@ export default function EventList({ city }) {
           const data = await response.json();
 
           if (data._embedded) {
+            //sorts the events by date
+            data._embedded.events.sort((a, b) => {
+              let dateA = new Date(a.dates.start.localDate);
+              let dateB = new Date(b.dates.start.localDate);
+              return dateA - dateB;
+            });
             setEvents(data._embedded.events);
           } else {
             setEvents([]);
@@ -32,27 +38,14 @@ export default function EventList({ city }) {
   }, [city]);
 
   return (
-
-    // <div>
-//   {events.map((event) => (
-//     <div key={event.id}>
-//       <h2>{event.name}</h2>
-//       <p>{event.dates.start.localDate}</p>
-//       <p>{event.dates.start.localTime}</p>
-//       <p>{event._embedded.venues[0].name}</p>
-//       <p>{event._embedded.venues[0].city.name}</p>
-//       <p>{event._embedded.venues[0].state.name}</p>
-//       <p>{event._embedded.venues[0].country.name}</p>
-//       <p>
-//         ----------------------------------------------------------------------
-//       </p>
-//     </div>
-
     <div>
       <ul role="list" className="divide-y divide-gray-100">
         {events.length > 0 ? (
           events.map((event) => (
-            <li key={event.id} className="flex justify-between gap-x-6 py-5">
+            <li
+              key={event.id}
+              className="flex justify-between gap-x-6 py-5 m-3"
+            >
               <div className="flex min-w-0 gap-x-4">
                 <img
                   alt=""
@@ -60,7 +53,7 @@ export default function EventList({ city }) {
                   className="h-12 w-12 flex-none rounded-full bg-gray-50"
                 />
                 <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
+                  <p className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-50">
                     {event.name}
                   </p>
                   <p className="mt-1 truncate text-xs leading-5 text-gray-500">
@@ -69,12 +62,14 @@ export default function EventList({ city }) {
                 </div>
               </div>
               <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className="text-sm leading-6 text-gray-900">
+                <p className="text-sm leading-6 text-gray-900 dark:text-gray-50">
                   {event._embedded.venues[0].city.name}
                 </p>
                 <p className="mt-1 text-xs leading-5 text-gray-500">
                   Time and Date <time>{event.dates.start.localDate}</time> at{" "}
-                  {event.dates.start.localTime}
+                  {event.dates.start.localTime
+                    ? event.dates.start.localTime.slice(0, 5)
+                    : "TBA"}
                 </p>
               </div>
             </li>
--- app/page.js
+++ app/page.js
@@ -12,15 +12,33 @@ export default function Home() {
   };
 
   return (
-    <main className="bg-slate-200 rounded-md">
+    <main className="bg-slate-300 rounded-md dark:bg-slate-800">
       <nav></nav>
-
       <div>
         <SearchBar onSearch={handleSearch} />
       </div>
+      <div className="flex justify-center gap-4 mt-4">
+        <p className="text-sm text-gray-900 dark:text-gray-300">
+          Choose a category:
+        </p>
+      </div>
+
+      <div className="flex justify-center gap-4 mt-4">
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          All
+        </button>
+        <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+          Music
+        </button>
+        <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+          Sports
+        </button>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          Arts & Theatre
+        </button>
+      </div>
 
       <div className="bg-slate-400 border-4 m-3 rounded-md"></div>
-
       <div className="row ml-3">
         <EventList city={city} />
       </div>
