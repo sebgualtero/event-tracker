9416add13d84e26fb9272bf331fbfcc8f8d1e492
Seb Gualtero <juansebastian.gualterovillamil@edu.sait.ca>
2024-08-05 22:48:58 -0600

    Revert "Authentication"
    This reverts commit 20dcd998baabd8fb4f800174e763865a1846bf81.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AuthContextProvider } from "./_utils/auth-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <AuthContextProvider>{children}</AuthContextProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
